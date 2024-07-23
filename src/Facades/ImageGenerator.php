e74af49dd02d8e461e8ff4fb5bcc48ffac2851f4
azlanali076 <azlanali076@gmail.com>
2024-07-23 23:18:43 +0500

    improved intellisense

--- src/Facades/ImageGenerator.php
+++ src/Facades/ImageGenerator.php
@@ -6,13 +6,13 @@ use Illuminate\Support\Facades\Facade;
 
 
 /**
- * @see \Azlanali076\ImageGenerator\ImageGenerator::RESPONSE_FORMAT_URL
- * @see \Azlanali076\ImageGenerator\ImageGenerator::RESPONSE_FORMAT_BASE64
- * @see \Azlanali076\ImageGenerator\ImageGenerator::STYLE_NATURAL
- * @see \Azlanali076\ImageGenerator\ImageGenerator::STYLE_VIVID
- * @see \Azlanali076\ImageGenerator\ImageGenerator::QUALITY_STANDARD
- * @see \Azlanali076\ImageGenerator\ImageGenerator::QUALITY_HD
- * @see \Azlanali076\ImageGenerator\ImageGenerator::generate()
+ * @property-read string QUALITY_STANDARD
+ * @property-read string QUALITY_HD
+ * @property-read string RESPONSE_FORMAT_URL
+ * @property-read string RESPONSE_FORMAT_BASE64
+ * @property-read string STYLE_NATURAL
+ * @property-read string STYLE_VIVID
+ * @method static array generate(string $prompt, int $width = 1024, int $height = 1024, string $quality = 'standard', string $style = 'vivid', string $responseFormat = 'url')
  */
 class ImageGenerator extends Facade {
 
